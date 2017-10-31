0eb41b77ec966d8103461172768afe989135db71
Jeff Olson <olson.jeffery@gmail.com>
2017-10-31 17:29:37 -0500

    setting up static publishing;shuffling file locations

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const fs = require('fs');
 data = {};
 
 app.get('/', (req, res) => {
-    var indexCompiled = _.template(fs.readFileSync('dist/index_template.html', 'utf8'));
+    var indexCompiled = _.template(fs.readFileSync('docs/index_template.html', 'utf8'));
     res.send(indexCompiled( { allData: data } ));
 });
-app.use(express.static('public'));
-app.use(express.static('dist'));
+app.use(express.static('docs'));
 app.use('src', express.static('src'));
 app.listen(3000, () => {
     data = JSON.stringify(parser.getAllData());
---
+++ genstatic.js
@@ -0,0 +1,8 @@
+const parser = require('./ruleset-parser');
+const _ = require('lodash');
+const fs = require('fs');
+
+var data = JSON.stringify(parser.getAllData());
+var indexCompiled = _.template(fs.readFileSync('docs/index_template.html', 'utf8'));
+var indexTxt = indexCompiled( { allData: data } );
+fs.writeFileSync('docs/index.html', indexTxt, 'utf8');
